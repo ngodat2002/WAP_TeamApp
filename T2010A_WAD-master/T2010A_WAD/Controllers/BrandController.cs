e89f520f79dcf6737d9b435581bf0b08f8e19c5e
ngodat2002 <73330656+ngodat2002@users.noreply.github.com>
2021-11-25 10:26:46 +0700

    up

---
+++ T2010A_WAD-master/T2010A_WAD/Controllers/BrandController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace T2010A_WAD.Controllers
+{
+    public class BrandController : Controller
+    {
+        // GET: Brand
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Update()
+        {
+            return View();
+        }
+        public ActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
---
+++ T2010A_WAD-master/T2010A_WAD/Controllers/CategoriesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace T2010A_WAD.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: Categories
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Update()
+        {
+            return View();
+        }
+
+    }
+}
